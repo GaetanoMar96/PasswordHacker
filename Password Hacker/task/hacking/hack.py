9c1723f444ed090148a2a01733699ce104378c1f
gaeta <gaetanomaranzino63@gmail.com>
2022-04-14 22:14:18 +0200

    passed test 3

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,7 +1,7 @@
+import os
+import itertools
 import socket
 import sys
-from string import ascii_lowercase, digits
-from itertools import product
 
 
 def receive_cmd():
@@ -13,27 +13,28 @@ def receive_cmd():
 def create_socket(HOST, PORT):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        create_pwd(s)
+        check_pwd(s)
 
 
-def create_pwd(s):
-    length = 1
-    is_on = True
-    while is_on:
-        pass_iter = product(letters_digits, repeat=length)
-        for pwd in pass_iter:
-            data = "".join(pwd)
-            msg = data.encode()
-            s.send(msg)
-            response = s.recv(1024)
-            response = response.decode()
+def check_pwd(s):
+    path = "C:\\Users\\gaeta\\PycharmProjects\\Password Hacker\\Password Hacker\\task\\hacking\\"
+    with open(path + "passwords.txt", "r", encoding="UTF-8") as f:
+        passwords = f.readlines()
+        for password in passwords:
+            password = password.strip()
+            for variation in map(lambda x: ''.join(x),
+                                 itertools.product(*([letter.lower(), letter.upper()] for letter in password))):
+                enc_variation = variation.encode()
+                s.send(enc_variation)
+                response = s.recv(1024)
+                response = response.decode("UTF-8")
+                if response == "Connection success!":
+                    print(variation)
+                    break
             if response == "Connection success!":
-                print(data)
-                is_on = False
                 break
-        length += 1
+
 
 
 HOST, PORT = receive_cmd()
-letters_digits = ascii_lowercase + digits
 create_socket(HOST, int(PORT))
